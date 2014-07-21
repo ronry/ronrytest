015c6e27f81f42b72aebaf2357a29388b3a9dda3
jinliang.lvjl <jinliang.ljl@alibaba-inc.com>
2014-07-21 22:05:53 +0800

    add jmx simple  test

---
+++ src/main/java/com/ronrytest/jmx/Hello.java
@@ -0,0 +1,27 @@
+package com.ronrytest.jmx;
+
+public class Hello implements HelloMBean {
+
+    private String name;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printHello() {
+        System.out.println("Hello World, " + name);
+    }
+
+    @Override
+    public void printHello(String whoName) {
+        System.out.println("Hello , " + whoName);
+    }
+
+}
---
+++ src/main/java/com/ronrytest/jmx/HelloMBean.java
@@ -0,0 +1,13 @@
+package com.ronrytest.jmx;
+
+public interface HelloMBean {
+
+    public String getName();
+
+    public void setName(String name);
+
+    public void printHello();
+
+    public void printHello(String whoName);
+
+}
